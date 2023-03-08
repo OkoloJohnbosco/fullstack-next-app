916a8de18fbc3282e2756edf43e91173a59ccd82
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-03-08 08:49:25 +0100

    chore: created auth and deashboard layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import GlassPane from "@/components/glass-pane";
+import "@/styles/global.css";
+import { Inter } from "next/font/google";
 
-function layout() {
-  return <div>layout</div>;
+const inter = Inter({ subsets: ["latin"] });
+
+export default function AuthRootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={inter.className}>
+      <head />
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center justify-center">
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  );
 }
-
-export default layout;
--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Signin() {
-  return <div>Signin</div>;
+  return (
+    <div>
+      <p className="text-[2xl] text-slate-400 ">Johnb</p>
+    </div>
+  );
 }
 
 export default Signin;
