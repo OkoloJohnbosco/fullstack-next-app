64cf80c8c671f2f2f80e53a6d51f136e11e51aa7
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-03-16 09:47:49 +0100

    chore: created loading state

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -9,7 +9,7 @@ import { delay } from "@/lib/async";
 import { getUserFromCookie } from "@/lib/auth";
 
 const getData = async () => {
-  await delay(2000);
+  // await delay(2000);
   const user = await getUserFromCookie(cookies());
   const projects = await db.project.findMany({
     where: {
