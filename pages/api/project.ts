401a1ae7bd21d3583cd1a0ddffb03cce7ee9e0c1
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-03-16 10:25:11 +0100

    chore: created create project endpoint

---
+++ pages/api/project.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { db } from "@/lib/db";
+import { validateJWT } from "@/lib/auth";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const user = await validateJWT(
+    req.cookies[process.env.COOKIE_NAME as string] as string
+  );
+
+  await db.project.create({
+    data: {
+      name: req.body.name,
+      ownerId: user.id,
+    },
+  });
+
+  res.json({ data: { message: "ok" } });
+}
